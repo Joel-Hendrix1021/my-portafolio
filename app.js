0611c1f8b515cb8924889d1dc6cb5d1fb8d783c3
Joel Pimentel <joelpimentel1995@gmail.com>
2021-03-18 22:55:45 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const $btnBars = document.getElementById("btn-menu");
+const $nav = document.querySelector(".nav");
+const $form = document.getElementById("form");
+const $reset = document.getElementById("reset");
+
+$btnBars.addEventListener("click", (e) => {
+  e.preventDefault();
+  $nav.classList.toggle("show");
+});
+
+ScrollReveal().reveal(".nav-menu");
+ScrollReveal().reveal(".main", { delay: 500 });
+ScrollReveal().reveal(".intro", { delay: 500 });
+ScrollReveal().reveal(".about", { delay: 500 });
+ScrollReveal().reveal(".skill", { delay: 500 });
+ScrollReveal().reveal(".proyect", { delay: 500 });
+ScrollReveal().reveal(".contact", { delay: 500 });
+
+//formulario de contacto
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = e.target.email.value;
+  const rexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  //
+  if (rexEmail.test(email)) {
+    fetch("https://formsubmit.co/ajax/joelpimentel1995@gmail.com", {
+      method: "POST",
+      body: new FormData(e.target)
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => console.log(json))
+      .catch(err => console.log(err));
+    
+     
+  } else {
+    console.log("no es valido");
+  }
+  e.target.name.value = ''
+  e.target.email.value = ''
+  e.target.message.value = ''
+  
+});
+
+
